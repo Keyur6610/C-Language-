749d31eb334584b7c80a9ccfb13e95548b94fdc5
Keyur6610 <120013936+Keyur6610@users.noreply.github.com>
2022-12-07 16:18:57 +0530

    Add files via upload

---
+++ PRINT_NAME.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name : Keyur Parekh\n");
+    printf("Birth Date : 09/01/2001\n");
+    printf("Age : 22\n");
+    printf("Address: Ahmedabad");
+
+    return 0;
+}
