10c3ab94e376fe9f15ba941757613dccd4f3d77e
Keyur6610 <120013936+Keyur6610@users.noreply.github.com>
2022-12-20 12:57:05 +0530

    Add files via upload

---
+++ ARRAY_SORT_ASC.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main ()
+{
+  int i, j, a, n = 5, num[5];
+
+
+  printf ("Enter The 5 Elements: \n");
+
+
+  for (i = 0; i < n; i++)
+    {
+
+      scanf ("%d", &num[i]);
+    }
+
+  for (i = 0; i < n; i++)
+    {
+
+      for (j = i + 1; j < n; j++)
+	{
+
+	  if (num[i] > num[j])
+	    {
+
+	      a = num[i];
+	      num[i] = num[j];
+	      num[j] = a;
+
+	    }
+
+	}
+
+    }
+
+  printf ("\nAscending Order\n");
+
+  for (i = 0; i < n; i++)
+    {
+
+      printf ("%d\n", num[i]);
+    }
+
+
+  return 0;
+}
